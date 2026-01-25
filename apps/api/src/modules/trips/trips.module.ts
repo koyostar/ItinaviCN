5423c88cd46ba12ce8197c1b269798db55a80a9b
Shermaine <shermaine.lsh@gmail.com>
2026-01-25 16:17:34 +0800

    fix(api): add PrismaModule import to TripsModule

--- apps/api/src/modules/trips/trips.module.ts
+++ apps/api/src/modules/trips/trips.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { TripsController } from './trips.controller';
 import { TripsService } from './trips.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TripsController],
   providers: [TripsService],
 })
