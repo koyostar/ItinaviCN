ce0f444a19f9479cda98f38f3e602611fe570d49
Shermaine <shermaine.lsh@gmail.com>
2026-01-25 18:18:28 +0800

    feat: add destinations json field to trips table

---
+++ apps/api/prisma/migrations/20260125092928_remove_default_exchange_rate/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `defaultExchangeRate` on the `Trip` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Trip" DROP COLUMN "defaultExchangeRate";
